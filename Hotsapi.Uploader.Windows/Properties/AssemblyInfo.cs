c8c2fa7ba67b6433f88a7507f7aacf0e4c1ec47e
Poma <Semenov.Roman@mail.ru>
2017-08-27 16:59:27 +0300

    Make readable title for correct shortcut names

--- Hotsapi.Uploader.Windows/Properties/AssemblyInfo.cs
+++ Hotsapi.Uploader.Windows/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hotsapi.Uploader.Windows")]
+[assembly: AssemblyProduct("Hotsapi Uploader")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
